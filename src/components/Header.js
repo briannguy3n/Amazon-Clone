ce61e1f9a4cf2c86950842bfccfc1eac625ae463
Brian Nguyen <nguyenbrian604@gmail.com>
2022-02-16 21:38:36 -0800

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "../styles/Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img
+        className="header_logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        alt=""
+      />
+
+      <div className="header_search">
+        <input type="text" className="header_searchInput" />
+        <SearchIcon className="header_searchIcon" />
+      </div>
+
+      <div className="header_nav">
+        <div className="header_option">
+          <span className="header_optionLineOne">Hello Guest</span>
+          <span className="header_optionLineTwo">Sign In</span>
+        </div>
+        <div className="header_option">
+          <span className="header_optionLineOne">Returns</span>
+          <span className="header_optionLineTwo">Add Orders</span>
+        </div>
+        <div className="header_option">
+          <span className="header_optionLineOne">Your</span>
+          <span className="header_optionLineTwo">Prime</span>
+        </div>
+
+        <div className="header_optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header_optionLineTwo header_basketCount">0</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Product.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/Product.css";
+
+function Product() {
+  return <div className="product"></div>;
+}
+
+export default Product;
