d6aa2a2e0bbf647d11413b914f3e82742d3dd310
Brian Nguyen <nguyenbrian604@gmail.com>
2022-02-17 10:16:28 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Home />
+      </div>
+    </Router>
   );
 }
 
