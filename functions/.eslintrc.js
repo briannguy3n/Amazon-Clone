bcef77a96c92afef7071093dcf55ab562f4b0def
Brian Nguyen <nguyenbrian604@gmail.com>
2022-02-18 14:04:55 -0800

    commit

---
+++ functions/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  parser: "@babel/eslint-parser",
+  parserOptions: {
+    requireConfigFile: false,
+  },
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "google"],
+  rules: {
+    quotes: ["error", "double"],
+    "max-len": ["error", { code: 140 }],
+  },
+};
